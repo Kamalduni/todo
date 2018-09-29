d2912d82b402652fb566987f7b0f1cf7732acbad
Mustapha Saman <samanmustapha@yahoo.com>
2018-09-29 04:00:03 -0400

    first commit

---
+++ app/controllers/todo_list_controller.rb
@@ -0,0 +1,20 @@
+class TodoListController < ApplicationController
+	def create
+		#render plain: params[:todo_list]. inspect
+		@post = todo_list.new(todo_params)
+		@post.save
+		redirect_to @post
+	end 
+
+	private def todo_params
+		params.require(:todo_list).permit(:title, :body)
+	end
+	def  show
+		@post = todo_list.find(params[:id])
+	end
+
+	def view
+		@post = todo_lists.all
+
+	end
+end
